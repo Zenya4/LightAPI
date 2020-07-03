57d0276676a6ae0b19029602d49f28d1030768f8
BuildTools <unconfigured@null.spigotmc.org>
2020-07-04 00:37:39 +0300

    [3.4.4] Added support for 1.16

--- plugin/src/main/java/ru/beykerykt/lightapi/LightAPI.java
+++ plugin/src/main/java/ru/beykerykt/lightapi/LightAPI.java
@@ -105,6 +105,7 @@ public class LightAPI extends JavaPlugin implements Listener {
 		craftbukkit.getVersions().put("v1_13_R2", CraftBukkit_v1_13_R2.class);
 		craftbukkit.getVersions().put("v1_14_R1", CraftBukkit_v1_14_R1.class);
 		craftbukkit.getVersions().put("v1_15_R1", CraftBukkit_v1_15_R1.class);
+		craftbukkit.getVersions().put("v1_16_R1", CraftBukkit_v1_16_R1.class);
 		ServerModManager.registerServerMod(craftbukkit);
 
 		ServerModInfo spigot = new ServerModInfo("Spigot");
@@ -118,6 +119,7 @@ public class LightAPI extends JavaPlugin implements Listener {
 		spigot.getVersions().put("v1_13_R2", CraftBukkit_v1_13_R2.class);
 		spigot.getVersions().put("v1_14_R1", CraftBukkit_v1_14_R1.class);
 		spigot.getVersions().put("v1_15_R1", CraftBukkit_v1_15_R1.class);
+		spigot.getVersions().put("v1_16_R1", CraftBukkit_v1_16_R1.class);
 		ServerModManager.registerServerMod(spigot);
 
 		ServerModInfo paperspigot = new ServerModInfo("PaperSpigot");
